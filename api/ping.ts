690481b45bce82c3700da322bf214e41273a61f7
aungsan-portfolio <53145303-littlebossh2o@users.noreply.replit.com>
2026-03-14 14:06:13 +0700

    debug: update ping api to include node version

--- api/ping.ts
+++ api/ping.ts
@@ -1,5 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.status(200).json({ ping: "pong", time: new Date().toISOString() });
+  return res.status(200).json({ 
+    ping: "pong", 
+    time: new Date().toISOString(),
+    nodeVersion: process.version
+  });
 }
